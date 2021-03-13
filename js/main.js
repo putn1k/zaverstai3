21e8f9000d8b169d2fa6afe9c89f37a5ef298455
putn1k <skachok11@yandex.ru>
2021-03-13 09:12:01 +0300

    добавляет валидацию телефонного номера

--- js/main.js
+++ js/main.js
@@ -16,3 +16,57 @@ $(function () {
     }
   });
 });
+
+//mask
+
+(function () {
+  let phoneNumber = document.querySelectorAll('[data-mask]');
+
+  if (phoneNumber) {
+    phoneNumber.forEach(function (el) {
+      el.addEventListener('input', function (event) {
+        showMask(event);
+      });
+    });
+  }
+
+  function showMask(event) {
+    const digitalPattern = /[0-9]/;
+    const literalPattern = /[0\*]/;
+    let input = event.target;
+    let value = input.value;
+    let mask = input.dataset.mask;
+    let newValue = '';
+
+    try {
+      let maskLength = mask.length;
+      let valueIndex = 0;
+      let maskIndex = 0;
+
+      for (; maskIndex < maskLength;) {
+        if (maskIndex >= value.length) {
+          break;
+        }
+
+        if (mask[maskIndex] === '0' && value[valueIndex].match(digitalPattern) === null) {
+          break;
+        }
+
+        while (mask[maskIndex].match(literalPattern) === null) {
+          if (value[valueIndex] === mask[maskIndex]) {
+            break;
+          }
+
+          newValue += mask[maskIndex++];
+        } //end cycle while
+
+        newValue += value[valueIndex++];
+        maskIndex++;
+      } //end cycle for
+
+      input.value = newValue;
+    } catch (evt) {
+      phoneNumber.removeEventListener();
+    }
+  }
+})();
